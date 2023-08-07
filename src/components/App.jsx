e3933bc0c8f64856eb0f79e879b685335e923926
Mayank <mayankbansal125@gmail.com>
2023-08-07 22:31:21 +0530

    Create editor and home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TextEditor from "./TextEditor";
 
 const App = () => {
   return (
-    <div>
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/editor" Component={TextEditor} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/TextEditor.jsx
@@ -0,0 +1,50 @@
+import React, { useCallback } from 'react';
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
+var toolbarOptions = [
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ 'font': [] }],
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'align': [] }],
+
+    [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+    [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+
+    ['clean']                                         // remove formatting button
+];
+
+const TextEditor = () => {
+    const wrapperRef = useCallback((wrapper) => {
+        if (wrapper == null) return;
+        wrapper.innerText = "";
+        const editor = document.createElement("div");
+        wrapper.append(editor);
+        new Quill(editor, {
+            modules: {
+                toolbar: toolbarOptions,
+                history: {
+                    delay: 2000,
+                    maxStack: 500,
+                    userOnly: true
+                }
+            },
+            theme: "snow"
+        });
+    }, []);
+
+    return (
+        <div className="text-editor" ref={wrapperRef}></div>
+    )
+}
+
+export default TextEditor;
