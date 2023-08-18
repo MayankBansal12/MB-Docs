3c7ef9c0151dc961c7c10e004261c071cdae43ee
Mayank <mayankbansal125@gmail.com>
2023-08-18 18:19:50 +0530

    Design home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,124 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <div className="container">
+        <Link className="tile" to="/editor">
+          <div className="icon">
+            <span class="material-symbols-outlined">add</span>
+          </div>
+        </Link>
+        <div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div>
+        <div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div>
+        <div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div><div className="tile">
+          <div className="icon edit">
+            <span class="material-symbols-outlined">edit</span>
+          </div>
+          <div className="content">
+            <div className="heading">Title for the document</div>
+            <div className="date">Created: 12/04/23</div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
